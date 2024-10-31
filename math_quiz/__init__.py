d323cd30b89be9e8803c4dbcab4358e158cf779a
sgshiva25 <sgshiva25@gmail.com>
2024-10-31 17:53:16 +0100

    Added initialization file

---
+++ math_quiz/__init__.py
@@ -0,0 +1,3 @@
+from math_quiz.math_quiz import get_random_number, get_random_operator, generate_problem, math_quiz
+from math_quiz.tests_math_quiz import TestMathGame
+from math_quiz.tests_math_quiz import test_function_A, test_function_B, test_function_C, test_get_random_number_invalid_range
